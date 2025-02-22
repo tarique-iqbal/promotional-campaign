8175414c844b00fdccffedd251b10823e004f24f
Tarique Iqbal <live.iqbal@gmail.com>
2025-02-23 00:21:21 +0100

    Add types.ts

---
+++ src/config/types.ts
@@ -0,0 +1,9 @@
+const TYPES = {
+  Promotion: Symbol.for('Promotion'),
+  BasketService: Symbol.for('BasketService'),
+  CheckoutService: Symbol.for('CheckoutService'),
+  PriceCalculatorService: Symbol.for('PriceCalculatorService'),
+  ExceptionHandler: Symbol.for('ExceptionHandler'),
+};
+
+export { TYPES };
